035411759cfa0e6e1caf95814c36f2ffa8f10bad
LeeHoseop <ghtjq3909@naver.com>
2021-01-28 19:10:06 +0900

    Json update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from queue import Queue, Empty
 from threading import Thread
 from werkzeug.utils import secure_filename
 import time
+import json
 import csv
 import os
 import io
@@ -23,12 +24,8 @@ CHECK_INTERVAL = 0.1
 # csv 파싱 길이제한 늘림
 csv.field_size_limit(100000000)
 
-UPLOAD_FOLDER = './csv_data/upload'
-RESULT_FOLDER = './csv_data/result'
-
-# make dir
-if not os.path.isdir('./csv_data'):
-    os.mkdir('./csv_data')
+UPLOAD_FOLDER = './data/upload'
+RESULT_FOLDER = './data/result'
 
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
@@ -48,17 +45,96 @@ def handle_requests_by_batch():
                 continue
 
             for requests in request_batch:
-                if len(requests['input']) == 1:
-                    requests["output"] = heading_parser(requests['input'][0])
-                elif len(requests['input']) == 3:
-                    requests["output"] = script_parser(requests['input'][0], requests['input'][1], requests['input'][2])
+                file_type = requests['input'].pop(0)
+
+                if file_type == 'csv':
+                    if len(requests['input']) == 1:
+                        requests["output"] = csv_heading_parser(requests['input'][0])
+                    elif len(requests['input']) == 3:
+                        requests["output"] = csv_script_parser(requests['input'][0], requests['input'][1], requests['input'][2])
+                elif file_type == 'json':
+                    if len(requests['input']) == 1:
+                        requests["output"] = json_heading_parser(requests['input'][0])
+                    elif len(requests['input']) == 3:
+                        requests["output"] = json_script_parser(requests['input'][0], requests['input'][1], requests['input'][2])
 
 
 handler = Thread(target=handle_requests_by_batch).start()
 
 
+def json_heading_parser(json_file):
+    try:
+        # 입력받은 파일 저장
+        filename = secure_filename(json_file.filename)
+        input_path = os.path.join(UPLOAD_FOLDER, filename)
+        json_file.save(input_path)
+
+        try:
+            with open(input_path, 'r', encoding='latin-1') as f:
+                json_data = json.load(f)
+        except:
+            with open(input_path, 'r', encoding='latin-1') as f:
+                json_data = [json.loads(line) for line in f]
+
+        for idx, data in enumerate(json_data):
+            if idx == 0:
+                x = list(data.items())
+                result = [[], []]
+
+                for i in x:
+                    result[0].append(i[0])
+                    result[1].append(i[1])
+
+                os.remove(input_path)
+                return result
+    except:
+        return jsonify({'message': 'Parsing error'}), 400
+
+
+def json_script_parser(json_file, name_idx, dialog_idx):
+    try:
+        # 입력받은 파일 저장
+        filename = secure_filename(json_file.filename)
+        input_path = os.path.join(UPLOAD_FOLDER, filename)
+        json_file.save(input_path)
+
+        filename = filename.split('.')[0] + '.txt'
+        result_path = os.path.join(RESULT_FOLDER, filename)
+
+        try:
+            with open(input_path, 'r', encoding='latin-1') as f:
+                json_data = json.load(f)
+        except:
+            with open(input_path, 'r', encoding='latin-1') as f:
+                json_data = [json.loads(line) for line in f]
+
+        with open(result_path, 'w', encoding='utf-8') as r:
+            for idx, line in enumerate(json_data):
+                # 이름이 없으면 Narrator 로 셋팅
+                who = " ".join(line[name_idx].split()) if line[name_idx] else 'Narrator'
+                dialog = " ".join(line[dialog_idx].split())
+
+                if dialog == '':
+                    continue
+
+                r.write(who + ': ' + dialog + '\n')
+
+        # 바이너리 정보로 전송하기 위함
+        with open(result_path, 'rb') as r:
+            data = r.read()
+
+        # 보안을 위한 정보 삭제
+        os.remove(input_path)
+        os.remove(result_path)
+
+        # heading_parser 와 다른 형태로 전송할 것이므로 구분을 위해 fake 를 추가함
+        return io.BytesIO(data), filename, 'fake'
+    except Exception:
+        return jsonify({'messege': 'Making error'})
+
+
 # CSV 파일의 meta 정보를 파싱
-def heading_parser(csv_file):
+def csv_heading_parser(csv_file):
     try:
         # 입력받은 파일 저장
         filename = secure_filename(csv_file.filename)
@@ -81,17 +157,21 @@ def heading_parser(csv_file):
 
 
 # csv 파일의 이름 인덱스와 대사 인덱스를 받아와서 스크립트 파일을 만듬
-def script_parser(csv_file, name_idx, dialog_idx):
+def csv_script_parser(csv_file, name_idx, dialog_idx):
     try:
         # 입력받은 파일 저장
         filename = secure_filename(csv_file.filename)
         input_path = os.path.join(UPLOAD_FOLDER, filename)
         csv_file.save(input_path)
 
+        filename = filename.split('.')[0] + '.txt'
+        result_path = os.path.join(RESULT_FOLDER, filename)
+
+        name_idx = int(name_idx)
+        dialog_idx = int(dialog_idx)
+
         with open(input_path, 'r', encoding='utf-8-sig') as f:
             rdr = csv.reader(f)
-            filename = filename.split('.')[0] + '.txt'
-            result_path = os.path.join(RESULT_FOLDER, filename)
 
             with open(result_path, 'w', encoding='utf-8') as r:
 
@@ -122,8 +202,8 @@ def script_parser(csv_file, name_idx, dialog_idx):
         return jsonify({'messege': 'Making error'})
 
 
-@app.route('/<types>', methods=['POST'])
-def csv_req(types):
+@app.route('/<file_types>/<types>', methods=['POST'])
+def csv_req(file_types, types):
     try:
         if types not in ['heading', 'script']:
             return jsonify({'message': 'Error. Wrong types'}), 400
@@ -131,13 +211,13 @@ def csv_req(types):
         if requests_queue.qsize() > BATCH_SIZE:
             return jsonify({'Error': 'Too Many Requests'}), 429
 
-        args = []
-        csv_file = request.files['csv_file']
-        args.append(csv_file)
+        args = [file_types]
+        file = request.files['file']
+        args.append(file)
 
         if types == 'script':
-            name = int(request.form['name'])
-            dialog = int(request.form['dialog'])
+            name = request.form['name']
+            dialog = request.form['dialog']
 
             args.append(name)
             args.append(dialog)
@@ -160,9 +240,9 @@ def csv_req(types):
 
 
 # 샘플 다운로드 링크
-@app.route('/sample_download')
+@app.route('/csv/sample_download')
 def send_sample():
-    return send_file('csv_data/sample.csv', mimetype='text/csv', attachment_filename='sample.csv'), 200
+    return send_file('data/sample.csv', mimetype='text/csv', attachment_filename='sample.csv'), 200
 
 
 ##
